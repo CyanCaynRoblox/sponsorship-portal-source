c42745e886d9d2f8e3bb90237d928b3066e0327d
CyanCayn <156147073+CyanCaynRoblox@users.noreply.github.com>
2025-08-20 00:11:12 -0500

    Update and rename billing.sql to accountManager.sql
    Signed-off-by: CyanCayn <156147073+CyanCaynRoblox@users.noreply.github.com>

---
+++ webhtml/backend/accountManager.sql
@@ -0,0 +1,7 @@
+/* this will be empty or a placeholder for now until i come up with something later (I ALSO DONT HAVE THE SQL SERVER YET)
+*/
+SELECT
+    name AS 'Username',
+    password_hash AS 'Password Hash'
+FROM
+    sys.sql_logins;
--- webhtml/backend/billing.sql
+++
@@ -1,3 +0,0 @@
-/* this will be empty for now until i come up with something later
-*/
-SELECT * FROM CUSTOMERS
